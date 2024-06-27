937b2b27bde972e6a59ddec28ee83d1fc0c7efc8
Boburjon <soliyevboburjon95@gmail.com>
2024-06-27 16:29:00 +0500

    init

--- src/main/java/com/example/spring_session_with_redis/controller/UserController.java
+++ src/main/java/com/example/spring_session_with_redis/controller/UserController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String helloWorld(){
-        return "hello world";
+        return "hello world 1";
     }
 }
