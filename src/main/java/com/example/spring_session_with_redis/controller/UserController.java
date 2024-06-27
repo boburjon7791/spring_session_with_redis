9ef58a8deb8c206a26c50da1b33412a56335845d
Boburjon <soliyevboburjon95@gmail.com>
2024-06-27 21:44:38 +0500

    Init

--- src/main/java/com/example/spring_session_with_redis/controller/UserController.java
+++ src/main/java/com/example/spring_session_with_redis/controller/UserController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/users")
+@RequestMapping("/api/users")
 public class UserController {
     @GetMapping
     public String helloWorld(){
-        return "hello world 1";
+        return "hello world 2";
     }
 }
