822ed0f1ba45217d139dea7062fbc00070cd796a
Boburjon <soliyevboburjon95@gmail.com>
2024-06-28 09:53:40 +0500

    last

--- src/main/java/com/example/spring_session_with_redis/config/SecurityConfig.java
+++ src/main/java/com/example/spring_session_with_redis/config/SecurityConfig.java
@@ -47,6 +47,10 @@ public class SecurityConfig {
                     formLogin.permitAll();
                     formLogin.failureForwardUrl("/");
                 })
+                .logout(logoutConfig -> {
+                    logoutConfig.logoutSuccessUrl("/logint");
+                    logoutConfig.logoutUrl("/api/users/logout");
+                })
                 .build();
     }
     @Bean
