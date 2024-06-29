fd5a1a7971ba87607874151849cf6fa156aa3f11
Boburjon <soliyevboburjon95@gmail.com>
2024-06-29 11:34:10 +0500

    last

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Java dasturini build qilish
+./gradlew clean build -x test
+
+# Docker login
+docker login -u bobur7761 -p bobur7761
+
+# Docker image'ni yaratish
+docker build -t bobur7761/spring_session .
+
+# Docker image'ni Docker Hub'ga push qilish
+docker push bobur7761/spring_session:latest
+
+# Docker Compose servisini to'xtatish
+docker-compose down
+
+# Docker image'larni yangilash
+docker-compose pull
+
+# Docker Compose servisini ishga tushirish
+docker-compose up -d
