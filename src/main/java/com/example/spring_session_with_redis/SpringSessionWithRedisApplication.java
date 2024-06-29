ccb1be42a16a99067b1f67b5c09e2eed651cc198
Boburjon <soliyevboburjon95@gmail.com>
2024-06-29 15:17:48 +0500

    Init

--- src/main/java/com/example/spring_session_with_redis/SpringSessionWithRedisApplication.java
+++ src/main/java/com/example/spring_session_with_redis/SpringSessionWithRedisApplication.java
@@ -2,13 +2,18 @@ package com.example.spring_session_with_redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableScheduling
 @SpringBootApplication
 public class SpringSessionWithRedisApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSessionWithRedisApplication.class, args);
 	}
