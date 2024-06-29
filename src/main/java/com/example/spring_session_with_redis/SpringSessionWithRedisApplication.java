6255ca02595ae267489d7abda71b809aaa4bf6b4
Boburjon <soliyevboburjon95@gmail.com>
2024-06-29 15:44:18 +0500

    last

--- src/main/java/com/example/spring_session_with_redis/SpringSessionWithRedisApplication.java
+++ src/main/java/com/example/spring_session_with_redis/SpringSessionWithRedisApplication.java
@@ -2,11 +2,7 @@ package com.example.spring_session_with_redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
